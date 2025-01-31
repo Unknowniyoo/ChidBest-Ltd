42083eb87a4ca1e8121d70582bc3e01fbe823487
Unknowniyoo <jameschidi12@gmail.com>
2025-01-31 13:32:18 -0800

    Update and rename script.js to Lesson
    1.Write a function called greet that takes a name as a parameter and returns "Hello, [name]!". 2.Add Two Numbers. create a function add that takes two numbers as parameters and returns their sum.
    3. Check Even or odd. Write a function is Even that takes a number as an argument and returns true if the number is even, otherwise false. 4.Square a Number. Create a function square that takes a number and returns its square.

--- work/script.js
+++
@@ -1,8 +0,0 @@
-function isEven(num) {
-  return num % 2 === 0;
-}
-console.log(isEven(4)); // true
-console.log(isEven(5)); // false
-console.log(isEven(0)); // true
-console.log(isEven(-2)); // true
-console.log(isEven(7)); // false
